60bcb311719cf353ed51e2dd2cc7056f35887129
layfr <laylafirsova@gmail.com>
2023-03-23 18:54:58 +0700

    fixes

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,25 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: './src/index.html',
+  }),
+  new MiniCssExtractPlugin({
+    filename: '[name].[contenthash].css',
+  }),
+];
 
 module.exports = {
   entry: './src/index.js',
+  plugins,
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  devtool: 'source-map',
+  devServer: {
+    hot: true,
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -12,6 +27,21 @@ module.exports = {
   },
   module: {
     rules: [
+      { test: /\.(html)$/, use: ['html-loader'] },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+        type: mode === 'production' ? 'asset' : 'asset/resource', // В продакшен режиме
+        // изображения размером до 8кб будут инлайнится в код
+        // В режиме разработки все изображения будут помещаться в dist/assets
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -29,11 +59,11 @@ module.exports = {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env', '@babel/preset-typescript'],
+              cacheDirectory: true,
             },
           },
         ],
       },
     ],
   },
-  plugins: [new HtmlWebpackPlugin()],
 };
